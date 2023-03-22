f880251e9b49dd1629bb9c41fc1c19f137836b1f
ira kevine <ikevine2016@gmail.com>
2023-03-23 00:44:48 +0200

    chore(configuration):postgres configuration and sequelize ORM

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
+import Sequelize from 'sequelize'
 
 dotenv.config()
 
@@ -9,17 +10,35 @@ const app =express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
-const {PORT,HOST}=process.env
+ const { PORT,HOST } = process.env;
 
+const sequelize = new  Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect:'postgres'
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 try {
-    app.listen(PORT,()=>{
-        console.log(`Server listening on port ${PORT}`)
-    })
+        app.listen(PORT,()=>{
+        console.log(`Server listening on port ${HOST}:${PORT}`)
+    
+      })
 } catch (error) {
     console.log(error.message)
 }
 
+
+ // app.listen(port()=>{
+    //     console.log(`Server listening on port ${port}`)
+    // })
 
 
 
