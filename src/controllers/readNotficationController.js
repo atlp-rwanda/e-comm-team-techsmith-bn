8f9365c6458fb0f1c24db44e5acd07383d09fbe3
Gabriel-mdv <mpuhwegabriel@gmail.com>
2023-04-26 12:34:11 +0200

    (feature): user mark notification as readNotficationController
    - user is able to mark notification as read
    - user can delete notification after reading them

---
+++ src/controllers/readNotficationController.js
@@ -0,0 +1,24 @@
+
+class markNotification{
+    static async read(req, res){
+        try{
+
+        }
+        catch(e){
+            res.status(500).json({
+                message: 'server Error'
+            })
+        }
+    }
+    static async deleteNotification(req, res){
+        try{
+
+        }
+        catch(e){
+            res.status(500).json({
+                message: 'server Error'
+            })
+        }
+
+    }
+}
